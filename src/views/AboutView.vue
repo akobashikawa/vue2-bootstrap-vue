46ab9f12d03d28db43d03ab26341aea23d17b560
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-07-08 02:14:45 -0500

    Fix component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <b-container fluid>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+  </b-container>
 </template>
